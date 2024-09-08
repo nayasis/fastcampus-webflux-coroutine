578b816cf857a26b1db8b94f1c9d495216541fc6
nayasis <nayasis@gmail.com>
2024-09-08 13:02:25 +0900

    디렉토리 변경

--- 02.spring-mvc/src/main/kotlin/dev/fastcampus/mvc/exception/NoArticleFound.kt
+++
@@ -1,8 +0,0 @@
-package dev.fastcampus.mvc.exception
-
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-import java.lang.RuntimeException
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-class NoArticleFound(message: String?): RuntimeException(message)
