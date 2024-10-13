eac35a19e72b73e4b6379d05d20d371df007bcfa
nayasis <nayasis@gmail.com>
2024-10-13 14:53:28 +0900

    live 코딩내용 추가

--- 02.mvc/src/test/kotlin/dev/fastcampus/mvc/service/ArticleServiceTest.kt
+++ 02.mvc/src/test/kotlin/dev/fastcampus/mvc/service/ArticleServiceTest.kt
@@ -15,62 +15,62 @@ import org.springframework.transaction.annotation.Transactional
 
 private val logger = KotlinLogging.logger {}
 
-@SpringBootTest
-@Transactional
-@Rollback
-@ActiveProfiles("test")
+@_root_ide_package_.org.springframework.boot.test.context.SpringBootTest
+@_root_ide_package_.org.springframework.transaction.annotation.Transactional
+@_root_ide_package_.org.springframework.test.annotation.Rollback
+@_root_ide_package_.org.springframework.test.context.ActiveProfiles("test")
 class ArticleServiceTest(
-    @Autowired private val service: ArticleService,
-    @Autowired private val repository: ArticleRepository,
+    @_root_ide_package_.org.springframework.beans.factory.annotation.Autowired private val service: _root_ide_package_.dev.fastcampus.mvc.service.ArticleService,
+    @_root_ide_package_.org.springframework.beans.factory.annotation.Autowired private val repository: _root_ide_package_.dev.fastcampus.mvc.repository.ArticleRepository,
 ) {
 
-    @Test
+    @_root_ide_package_.org.junit.jupiter.api.Test
     fun `create & get`() {
-        val req = ReqCreate("title 1", "blabla 1", 1234)
+        val req = _root_ide_package_.dev.fastcampus.mvc.service.ReqCreate("title 1", "blabla 1", 1234)
         val id  = service.create(req).id
         service.get(id).let {
-            assertEquals(req.title, it.title)
-            assertEquals(req.body, it.body)
-            assertEquals(req.authorId, it.authorId)
+            _root_ide_package_.org.junit.jupiter.api.Assertions.assertEquals(req.title, it.title)
+            _root_ide_package_.org.junit.jupiter.api.Assertions.assertEquals(req.body, it.body)
+            _root_ide_package_.org.junit.jupiter.api.Assertions.assertEquals(req.authorId, it.authorId)
         }
-        logger.debug { ">> count: ${repository.count()}" }
+        _root_ide_package_.dev.fastcampus.mvc.service.logger.debug { ">> count: ${repository.count()}" }
     }
 
-    @Test
+    @_root_ide_package_.org.junit.jupiter.api.Test
     fun `get - not found`() {
-        assertThrows<NotFoundException> { service.get(99999999)  }
-        logger.debug { ">> count: ${repository.count()}" }
+        _root_ide_package_.org.junit.jupiter.api.assertThrows<_root_ide_package_.dev.fastcampus.mvc.exception.NotFoundException> { service.get(99999999)  }
+        _root_ide_package_.dev.fastcampus.mvc.service.logger.debug { ">> count: ${repository.count()}" }
     }
 
-    @Test
+    @_root_ide_package_.org.junit.jupiter.api.Test
     fun getAll() {
-        service.create(ReqCreate("title1"))
-        service.create(ReqCreate("title2"))
-        service.create(ReqCreate("title matched"))
+        service.create(_root_ide_package_.dev.fastcampus.mvc.service.ReqCreate("title1"))
+        service.create(_root_ide_package_.dev.fastcampus.mvc.service.ReqCreate("title2"))
+        service.create(_root_ide_package_.dev.fastcampus.mvc.service.ReqCreate("title matched"))
 
-        assertEquals(3, service.getAll().size)
-        assertEquals(1, service.getAll("matched").size)
-        assertEquals(0, service.getAll("none").size)
+        _root_ide_package_.org.junit.jupiter.api.Assertions.assertEquals(3, service.getAll().size)
+        _root_ide_package_.org.junit.jupiter.api.Assertions.assertEquals(1, service.getAll("matched").size)
+        _root_ide_package_.org.junit.jupiter.api.Assertions.assertEquals(0, service.getAll("none").size)
 
-        logger.debug { ">> count: ${repository.count()}" }
+        _root_ide_package_.dev.fastcampus.mvc.service.logger.debug { ">> count: ${repository.count()}" }
     }
 
-    @Test
+    @_root_ide_package_.org.junit.jupiter.api.Test
     fun update() {
-        val new = service.create(ReqCreate("title 1", "blabla 1", 1234))
-        service.update(new.id, ReqUpdate(authorId = 9999))
-        assertEquals(9999, service.get(new.id).authorId)
+        val new = service.create(_root_ide_package_.dev.fastcampus.mvc.service.ReqCreate("title 1", "blabla 1", 1234))
+        service.update(new.id, _root_ide_package_.dev.fastcampus.mvc.service.ReqUpdate(authorId = 9999))
+        _root_ide_package_.org.junit.jupiter.api.Assertions.assertEquals(9999, service.get(new.id).authorId)
     }
 
-    @Test
+    @_root_ide_package_.org.junit.jupiter.api.Test
     fun delete() {
         val prev = repository.count()
-        val created = service.create(ReqCreate("title 1", "blabla 1", 1234))
-        assertEquals(prev + 1, repository.count())
+        val created = service.create(_root_ide_package_.dev.fastcampus.mvc.service.ReqCreate("title 1", "blabla 1", 1234))
+        _root_ide_package_.org.junit.jupiter.api.Assertions.assertEquals(prev + 1, repository.count())
         service.delete(created.id)
-        assertEquals(prev, repository.count())
+        _root_ide_package_.org.junit.jupiter.api.Assertions.assertEquals(prev, repository.count())
 
-        logger.debug { ">> count: ${repository.count()}" }
+        _root_ide_package_.dev.fastcampus.mvc.service.logger.debug { ">> count: ${repository.count()}" }
     }
 
 }
