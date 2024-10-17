7bdae19e42262d23b11377463ae12d80a366facd
nayasis <nayasis@gmail.com>
2024-10-17 23:03:39 +0900

    composite key 오류 수정

--- 02.mvc-live/src/main/kotlin/dev/fastcampus/mvc/model/CompositePkEntity.kt
+++ 02.mvc-live/src/main/kotlin/dev/fastcampus/mvc/model/CompositePkEntity.kt
@@ -1,24 +1,28 @@
 package dev.fastcampus.mvc.model
 
 import jakarta.persistence.Column
+import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.IdClass
 import java.io.Serializable
 
+@Entity(name = "TB_COMPOSITE_KEY")
+@IdClass(CompositePkEntity.pk::class)
 class CompositePkEntity {
 
     @Id
-    var id: String? = null
+    @Column(name="`key`")
+    var key: String? = null
 
     @Id
+    @Column(name="`type`")
     var type: String? = null
 
-    @Column
+    @Column(name="`value`")
     var value: String? = null
 
     data class pk (
-        @Column(name="id")
-        val id: String,
-        @Column(name="type")
+        val key: String,
         val type: String,
     ): Serializable
 
